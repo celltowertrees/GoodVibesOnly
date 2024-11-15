1d3d6ff1039d9fe8fa7b7a41194ddd8e9f8273fc
Kelly Monson <kellymonson@signalnoise.io>
2024-11-15 23:00:20 +0000

    some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -42,9 +42,11 @@ export default function Home() {
     <div className="m-10">
       <h1>✨ Good Vibes Only ✨</h1>
       <hr className="my-10" />
-      <div className="w-full flex">
-        <input type="text" className="flex-1" placeholder="Everything Sucks!!!" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
-        <button onClick={() => generateThing()}>Post</button>
+      <div className="w-full">
+        <form className="w-full flex" onSubmit={(e) => { e.preventDefault(); generateThing() } }>
+          <input type="text" className="flex-1" placeholder="Everything Sucks!!!" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+          <button type="submit">Post</button>
+        </form>
       </div>
       <hr className="my-10" />
       {loading && <p>Loading...</p>}
